4f2a367579e19a48828f6db25d57529ee0a55562
lauraoliveri <lauraoliveri2003@gmail.com>
2025-06-17 11:50:53 +0200

    Esercizio completato

--- src/Esercizio.java
+++ src/Esercizio.java
@@ -1,2 +1,29 @@
 public class Esercizio {
+    public static void main(String[] args) {
+
+        int n = 7;
+        for (int i = 0; i <= n ; i++) {
+            for (int j = i; j > 0 ; j--) {
+                System.out.print(j);
+            }
+
+            for (int k = 2 ; k <= i ; k++) {
+                System.out.print(k);
+            }
+            System.out.println("");
+
+        }
+
+        for (int i = n-1 ; i > 0 ; i--) {
+            for (int j = i ; j > 0 ; j--) {
+                System.out.print(j);
+            }
+
+            for (int k = 2 ; k <= i ; k++) {
+                System.out.print(k);
+            }
+            System.out.println("");
+        }
+
+    }
 }
